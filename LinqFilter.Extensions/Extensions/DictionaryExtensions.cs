91b619e0cadd25fb9d5aa81850f04e966d2752a3
James Dunne <james.jdunne@gmail.com>
2011-03-21 14:59:46 +0000

    Added GetValueOr dictionary extension methods.

---
+++ LinqFilter.Extensions/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Collections.Generic
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetValueOr<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue defaultValue)
+        {
+            if (dict == null) return defaultValue;
+
+            TValue value = defaultValue;
+            dict.TryGetValue(key, out value);
+            return value;
+        }
+
+        public static TValue GetValueOr<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, Func<TValue> getDefaultValue)
+        {
+            if (dict == null) return getDefaultValue();
+
+            TValue value;
+            if (dict.TryGetValue(key, out value))
+            {
+                return value;
+            }
+
+            return getDefaultValue();
+        }
+    }
+}
