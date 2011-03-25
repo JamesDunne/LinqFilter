a45d417d71a7de1b1be299b45cbb0fea8d0e8072
James Dunne <james.jdunne@gmail.com>
2011-03-25 20:57:59 +0000

    Adding Mutate<T> extension methods.

---
+++ LinqFilter.Extensions/Extensions/MutationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class MutationExtensions
+    {
+        public static T Mutate<T>(this T[] array, int index, T setValue)
+        {
+            array[index] = setValue;
+            return setValue;
+        }
+
+        public static T Mutate<T>(this IList<T> list, int index, T setValue)
+        {
+            list[index] = setValue;
+            return setValue;
+        }
+
+        public static T MutateAdd<T>(this IList<T> list, T setValue)
+        {
+            list.Add(setValue);
+            return setValue;
+        }
+
+        public static TValue Mutate<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue setValue)
+        {
+            dict[key] = setValue;
+            return setValue;
+        }
+    }
+}
